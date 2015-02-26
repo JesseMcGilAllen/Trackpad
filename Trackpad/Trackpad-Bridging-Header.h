76596cb59284adecf47c4d05023a54658441eb21
Jesse McGil Allen <jmallen7@gmail.com>
2015-02-25 18:01:33 -0600

    adding Touchpose to project

---
+++ Trackpad/Trackpad-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Trackpad-Bridging-Header.h
+//  Trackpad
+//
+//  Created by Jesse McGil Allen on 2/25/15.
+//  Copyright (c) 2015 Jesse McGil Allen. All rights reserved.
+//
+
+#ifndef Trackpad_Trackpad_Bridging_Header_h
+#define Trackpad_Trackpad_Bridging_Header_h
+
+#import "QTouchposeApplication.h"
+
+#endif
---
+++ Trackpad/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Move
+//
+//  Created by Jesse McGil Allen on 1/27/15.
+//  Copyright (c) 2015 Jesse McGil Allen. All rights reserved.
+//
+
+import Foundation
+
+UIApplicationMain(Process.argc, Process.unsafeArgv, NSStringFromClass(QTouchposeApplication.self), NSStringFromClass(AppDelegate.self) )
+
